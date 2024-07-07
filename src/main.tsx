13dcac0a2a9813726d10ce141a32ab6872747de3
Yaraslava Adzinets <yaroslava.odin.00@gmail.com>
2024-07-07 16:27:17 +0300

    feat: add ErrorBoundary.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Main from "./components/search-input/Main.tsx";
+import Main from "./components/main/Main.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
