202e0c87c4ed52b0c757703d807be2ec07f963d4
Yaraslava Adzinets <yaroslava.odin.00@gmail.com>
2024-07-07 06:22:37 +0300

    feat: add search.

---
+++ src/components/search-input/Main.tsx
@@ -0,0 +1,113 @@
+import { Component, ReactNode } from "react";
+import "./Main.css";
+
+interface DefaultState {
+  searchQuery: string;
+  result: [];
+  loading: boolean;
+}
+
+interface Character {
+  uid: number;
+  name: string;
+  gender: string;
+  yearOfBirth: number;
+  yearOfDeath: number;
+}
+
+interface Result {
+  characters: [];
+}
+
+class Main extends Component {
+  constructor(props: []) {
+    super(props);
+
+    this.state = {
+      searchQuery: "",
+      result: [],
+      loading: true,
+    };
+
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  public async componentDidMount(): Promise<void> {
+    const { searchQuery } = this.state as DefaultState;
+    await this.fetchData(searchQuery);
+  }
+
+  private async fetchData(searchQuery: string): Promise<void> {
+    this.setState({ loading: true });
+
+    if (searchQuery) {
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          accept: "application/json",
+        },
+        body: `name=${searchQuery}`,
+      };
+
+      await fetch(
+        "https://stapi.co/api/v1/rest/character/search?pageNumber=1",
+        requestOptions,
+      )
+        .then((response) => response.json())
+        .then((result) => this.setState({ result: result, loading: false }));
+    } else {
+      await fetch("https://stapi.co/api/v1/rest/character/search?pageNumber=1")
+        .then((response) => response.json())
+        .then((result) => this.setState({ result: result, loading: false }));
+    }
+  }
+
+  public render(): ReactNode {
+    const { searchQuery, result, loading } = this.state as DefaultState;
+    const listItems = (result as unknown as Result)?.characters?.map(
+      (person: Character) => (
+        <div key={person.uid} className="card">
+          <b className="card_info">{person.name}</b>
+          <span className="card_info">Gender : {person.gender}</span>
+          <span className="card_info">
+            Year of birth : {person.yearOfBirth}
+          </span>
+          <span className="card_info">
+            Year of death : {person.yearOfDeath}
+          </span>
+        </div>
+      ),
+    );
+
+    return (
+      <div>
+        <div className="title">Search for Star Trek characters</div>
+        <div className="search-container">
+          <div className="input">
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={(event) =>
+                this.setState({ searchQuery: event.target.value })
+              }
+              placeholder="Start search..."
+            />
+
+            <button className="btn" onClick={() => this.fetchData(searchQuery)}>
+              Search
+            </button>
+          </div>
+        </div>
+
+        {loading ? (
+          <div className="loader">Loading...</div>
+        ) : (
+          <div className="cards-container">{listItems}</div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Main;
